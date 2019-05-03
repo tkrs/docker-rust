6ef89652df4d56bc9411cbd75bc84b63085a1f4c
Takeru Sato <type.in.type@gmail.com>
2019-05-04 01:18:06 +0900

    Change how to install kcov

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -eux
 
 mkdir /workspace; cd /workspace
 
-apt-get update && apt-get install -y libcurl4-openssl-dev libelf-dev libdw-dev binutils-dev cmake gcc
+apt-get update && apt-get install -y cmake g++ pkg-config jq libcurl4-openssl-dev libelf-dev libdw-dev binutils-dev libiberty-dev
 
 curl -L -OsS "https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init"
 chmod +x rustup-init
@@ -11,14 +11,9 @@ chmod +x rustup-init
 chmod -R a+w $RUSTUP_HOME $CARGO_HOME
 rustup component add clippy rustfmt
 
-kcov_version=36
-curl -L -OsS "https://github.com/SimonKagstrom/kcov/archive/v${kcov_version}.tar.gz"
-tar -xf v${kcov_version}.tar.gz
-cd kcov-${kcov_version}
-mkdir build; cd build
-cmake .. && make && make install
-which kcov
 cargo install cargo-kcov
+cargo kcov --print-install-kcov-sh | sh
+
 rustup show
 cargo --version
 
